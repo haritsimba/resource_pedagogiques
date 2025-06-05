5b4ff629df8da5dddd29c00895c7460639baae09
haritsimba <haritsimbafranck@gmail.com>
2025-06-05 13:27:17 +0300

    repository initalization

---
+++ src/main/java/com/project/poo/rp/RpApplication.java
@@ -0,0 +1,13 @@
+package com.project.poo.rp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpApplication.class, args);
+	}
+
+}
